94ab2f55bd1c99c66473744d311dcc9b669d5325
VinnieIT <mwangivin3@gmail.com>
2021-08-26 11:38:05 +0300

    Add DB connection class

---
+++ src/test/java/Dao/Sql2oDeptDaoTest.java
@@ -0,0 +1,62 @@
+package Dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Sql2oDeptDaoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void addUserToDept() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void allDepartments() {
+    }
+
+    @Test
+    void allDepartmentEmployees() {
+    }
+
+    @Test
+    void allDepartmentNews() {
+    }
+
+    @Test
+    void deleteDepartmentById() {
+    }
+
+    @Test
+    void deleteEmployeeFromDept() {
+    }
+
+    @Test
+    void deleteDeptNewsById() {
+    }
+
+    @Test
+    void deleteAll() {
+    }
+
+    @Test
+    void updateEmployeeCount() {
+    }
+}
